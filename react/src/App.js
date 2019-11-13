6fe7632f3002ad0d57f96a3fe7898136873074d4
brianxautumn <shred@brianautumn.com>
2019-11-12 21:22:38 -0800

    Add front end

---
+++ react/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const [clickCount, setClickCount] = React.useState(0);
+  const ws = React.useRef(new WebSocket(`ws://${window.location.host}/ws`)); // don't make a new one every time it renders
+
+  ws.current.onopen = () => {
+    console.log('Connection open!')
+  };
+
+  ws.current.onmessage = (message) => {
+    console.log(message);
+    setClickCount(Number(message.data));
+  };
+
+  ws.current.onclose = () => {
+    console.log('connection closed');
+  };
+
+  ws.current.onerror = () => {
+    console.log('ws error');
+  };
+
+  const handleClick = () => {
+    ws.current.send(clickCount + 1);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={handleClick} className="clicker">
+          {clickCount}
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
