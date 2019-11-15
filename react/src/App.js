6fdc41fcb2d66bd37b2d1653c12aee55b8800e73
brianxautumn <shred@brianautumn.com>
2019-11-14 20:27:04 -0800

    Fix old ws connection

--- react/src/App.js
+++ react/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
 
+const wsSession = new WebSocket(`ws://localhost:1235/ws`); // fix for multiple connections
+//new WebSocket(`ws://${window.location.host}/ws`) for aws
+
 function App() {
   const [clickCount, setClickCount] = React.useState(0);
-  const ws = React.useRef(new WebSocket(`ws://${window.location.host}/ws`)); // don't make a new one every time it renders
+  const ws = React.useRef(wsSession); // don't make a new one every time it renders
 
   ws.current.onopen = () => {
     console.log('Connection open!')
